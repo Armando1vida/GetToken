48767a9d7a20d65027e27487d5a068c71d29cc3f
ArmandoConejo <armand1live@gmail.com>
2014-10-04 21:01:18 -0500

    Test method of getToken

--- GetToken/src/Run.java
+++ GetToken/src/Run.java
@@ -14,7 +14,7 @@ public class Run {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Token to = new Token("12Hola hola83+s00+-9.23/*");
+        Token to = new Token("12H/ola hola83+s00+9/5.3hola-9.2h3/9*");
         while (to.getHaveToken()) {
             String r = to.getToken();
             if (r.length() > 0) {
